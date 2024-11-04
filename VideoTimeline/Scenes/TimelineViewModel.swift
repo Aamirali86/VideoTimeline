df6637a67280b492532b389c91f3270599709c45
aamir <aamir-muhammad@hotmail.com>
2024-11-05 03:23:39 +0500

    basic app setup

---
+++ VideoTimeline/Scenes/TimelineViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  VideoTimelineViewModel.swift
+//  VideoTimeline
+//
+//  Created by Aamir on 05/11/2024.
+//
+
+import Foundation
+
+final class VideoTimelineViewModel {
+    
+}
---
+++ VideoTimeline/Scenes/VideoTimelineController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  VideoTimeline
+//
+//  Created by Aamir on 04/11/2024.
+//
+
+import UIKit
+
+final class VideoTimelineController: UIViewController {
+    var viewModel: VideoTimelineViewModel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+    }
+}
+
+// MARK: UI setup
+extension VideoTimelineController {
+    
+}
